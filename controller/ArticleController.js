93fa490be71ca27335eadfd6fac782956ddd14ae
tungjav <tungchuyenthainguyen@gmail.com>
2023-02-01 09:15:23 +0700

    first

--- controller/ArticleController.js
+++ controller/ArticleController.js
@@ -2,11 +2,11 @@ const Article = require("../model/Article");
 
 module.exports.createArticle = async (req, res) => {
   try {
-    const article = await new Article({
-      ...req.body,
-    });
-    article.save();
-    res.status(200).json(article);
+    for (const article of req.body) {
+      const element = await new Article({ ...article });
+      element.save();
+    }
+    res.status(200).json(req.body);
     return;
   } catch (error) {
     res.status(500).json(error);
