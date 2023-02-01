bb242154460af7a23b4cf1ae67b72f5433a70811
phamson02 <phamtienson02@gmail.com>
2023-02-02 00:30:21 +0700

    update

--- router/index.js
+++ router/index.js
@@ -4,8 +4,59 @@ const NodeController = require("../controller/NodeController");
 const ArticleController = require("../controller/ArticleController");
 const EdgeController = require("../controller/EdgeController");
 
-appRoute.get("/", (req, res) => {
-  res.send("Hello World!");
+appRoute.get("/", async (req, res) => {
+  let nodes = await NodeController.find();
+  let edges = await EdgeController.find();
+  nodes = nodes.map((node) => {
+    return {
+      key: node._id,
+      name: node.name,
+      type: node.type,
+      cluster: node.cluster,
+      score: node.score,
+    };
+  });
+  edges = edges.map((edge) => {
+    return {
+      key: edge._id,
+      source: edge.source,
+      target: edge.target,
+      size: edge.size,
+      articles: edge.articles.map((article) => {
+        return {
+          key: article._id,
+          title: article.title,
+          url: article.url,
+          date: article.pubDate,
+        };
+      }),
+    };
+  });
+  return res.status(200).json({
+    nodes: nodes,
+    edges: edges,
+    cluster: [
+      {
+        key: "0",
+        color: "#6c3e81",
+        clusterLabel: "Other entities",
+      }
+    ],
+    tags: [
+      {
+        key: "PER",
+        image: "person.svg",
+      },
+      {
+        key: "ORG",
+        image: "organization.svg",
+      },
+      {
+        key: "LOC",
+        image: "unknown.svg",
+      },
+    ]
+  });
 });
 
 appRoute.post("/node", NodeController.createNode);
