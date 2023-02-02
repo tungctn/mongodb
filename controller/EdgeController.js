1ee3ba2125ed94ab14a3ddaca52f96df7c1a1ba0
phamson02 <phamtienson02@gmail.com>
2023-02-02 11:56:01 +0700

    update EdgeController.js

--- controller/EdgeController.js
+++ controller/EdgeController.js
@@ -2,29 +2,19 @@ const Edge = require("../model/Edge");
 
 module.exports.createEdge = async (req, res) => {
   try {
-    let arr_source = [];
-    let arr_target = [];
-    const listEdge = await Edge.find();
-    for (const edge of listEdge) {
-      arr_source.push(edge.source.toString());
-      arr_target.push(edge.target.toString());
-    }
-
     for (const edge of req.body) {
-      console.log(edge.source);
-      console.log(edge.target);
-      if (
-        (arr_source.includes(edge.source) && arr_target.includes(edge.target)) ||
-        (arr_source.includes(edge.target) && arr_target.includes(edge.source))
-      ) {
-        // Find existing edge
-        const existEdge = await Edge.findOne({
-          $or: [
-            { source: edge.source, target: edge.target },
-            { source: edge.target, target: edge.source },
-          ],
-        });
-        let articles = existEdge.articles.map((article) => article.toString());
+      console.log(edge);
+
+      // Find existing edge
+      const existEdge = await Edge.findOne({
+        $or: [
+          { source: edge.source, target: edge.target },
+          { source: edge.target, target: edge.source },
+        ],
+      });
+      console.log(existEdge);
+      if (existEdge) {
+        let articles = existEdge.articles?.map((article) => article.toString());
         articles = articles.concat(edge.articles);
         articles = articles.filter(
           (item, index) => articles.indexOf(item) === index
@@ -32,9 +22,11 @@ module.exports.createEdge = async (req, res) => {
         existEdge.articles = articles;
         existEdge.size = articles.length;
         existEdge.save();
+        console.log("Update edge successfully");
       } else {
         const element = await new Edge({ ...edge });
         element.save();
+        console.log("Create edge successfully");
       }
     }
     res.status(200).json({
